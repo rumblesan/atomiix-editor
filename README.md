64be32600be38a8bcc3280217f475db7bc4150e6
Guy John <guy@rumblesan.com>
2019-03-06 22:01:04 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Atomiix
+
+Editor for the [Atomiix](https://github.com/rumblesan/atomiix) environment.
+
+## Functionality
+
+Sends either the single line the cursor is on, or all selected text.
+
+* `Ctrl-Enter`
+
+## Filetype
+
+The plugin will only be usable for files with a *.ixi* extension.
+
+## Contact
+
+Drop me an email at [guy@rumblesan.com](mailto:guy@rumblesan.com).
+
+## License
+
+BSD License.
---
+++ menus/atomiix.js
@@ -0,0 +1,26 @@
+{
+  "context-menu": {
+    "atom-text-editor": [
+      {
+        "label": "Send to Atomiix",
+        "command": "atomiix:send"
+      }
+    ]
+  },
+  "menu": [
+    {
+      "label": "Packages",
+      "submenu": [
+        {
+          "label": "Atomiix",
+          "submenu": [
+            {
+              "label": "Send",
+              "command": "atomiix:send"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
